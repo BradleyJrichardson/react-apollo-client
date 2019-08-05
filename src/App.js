41f8d6ca5e1018b58ab7296e779dfb5555696d22
BradleyJrichardson <brdl.richardson@gmail.com>
2019-08-05 10:51:53 +1000

    components, routes, nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Nav from "./components/Nav";
+import { Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>hi world</h1>
-    </div>
+    <Router>
+      <Container>
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
